cc505908e82568fcd172425661d5cb9f8e03f846
Alex Rybtsov <alexander.rybtsov@gmail.com>
2023-03-18 14:10:53 +0300

    new

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+
+
 public class Main {
+    private static MagicBox<String> StringMagicBox;
+
     public static void main(String[] args) {
-        System.out.println("aaaaa!");
+        int i = 4;
+        MagicBox<String> stringMagicBox = new MagicBox<>(size:4);
+        StringMagicBox.add("123");
+        StringMagicBox.add("qwerty");
+        StringMagicBox.add("someString");
+        StringMagicBox.add("magicString");
+
+        System.out.println (pick);
     }
+    }
+
+
 }
